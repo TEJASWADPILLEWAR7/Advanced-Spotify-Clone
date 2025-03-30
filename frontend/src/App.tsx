a49ee5b9a1df18e894aa6c76e2454ab599e7acd4
TEJASWADPILLEWAR7 <tejaswadpillewar7@gmail.com>
2025-03-30 21:47:44 +0530

    Initial commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+function App() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-svh">
+      <Button>This is my first shadCN componenet</Button>
+    </div>
+  );
+}
+
+export default App;
